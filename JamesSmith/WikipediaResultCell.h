e26c33128034f017b157522b262e8dd3e28ea8a3
James Smith <jmecsmith@gmail.com>
2014-04-24 19:16:00 -0400

    Renamed ResultCell to BasicResultCell. Added WikipediaResultCell class

---
+++ JamesSmith/WikipediaResultCell.h
@@ -0,0 +1,13 @@
+//
+//  WikipediaResultCellTableViewCell.h
+//  Seek
+//
+//  Created by admin on 4/24/14.
+//  Copyright (c) 2014 James Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WikipediaResultCellTableViewCell : UITableViewCell
+
+@end
---
+++ JamesSmith/WikipediaResultCell.m
@@ -0,0 +1,34 @@
+//
+//  WikipediaResultCellTableViewCell.m
+//  Seek
+//
+//  Created by admin on 4/24/14.
+//  Copyright (c) 2014 James Smith. All rights reserved.
+//
+
+#import "WikipediaResultCellTableViewCell.h"
+
+@implementation WikipediaResultCellTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
